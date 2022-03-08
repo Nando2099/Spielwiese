e560ff773a12080c5d73985ef8e303f42cbb3f19
Nando2099 <101092641+Nando2099@users.noreply.github.com>
2022-03-08 15:39:17 +0100

    primer commit

---
+++ src/Read_Files_Aufgabe27_2_C_nando/Main.java
@@ -0,0 +1,71 @@
+package Read_Files_Aufgabe27_2_C_nando;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// public - it can be called from anywhere
+// static - it doesn't have any object state, so you can call it without instantiating an object
+// void - it doesn't return anything
+//File reading con FileNotFoundException en vez de Try Catch, linea 9!
+
+public class Main {
+
+	
+
+	
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		String dateiadresse = "src/Read_Files_Aufgabe27_2_C_nando/daten1.txt";
+		String dateiadresse2 = "src/Read_Files_Aufgabe27_2_C_nando/daten2.txt";
+		
+		ArrayList<String> etwas = new ArrayList<>();
+		ArrayList<String> etwas2 = new ArrayList<>();
+
+		
+		etwas.add(dateiadresse);
+		etwas2.add(dateiadresse2);
+		
+		
+		leseDateiAnUndZeigeSieAn(etwas);
+		leseDateiAnUndZeigeSieAn(etwas2);
+		
+
+	}
+
+	
+			// METODO 
+	
+	
+		public static void leseDateiAnUndZeigeSieAn(ArrayList<String> etwas ) throws FileNotFoundException{
+			// System.out.println(etwas); hasta aqui ok
+			
+			String str = "";
+			
+			for (String s : etwas) {
+			    str+= s;
+			    
+			}
+			
+			//System.out.println(str);// hasta aaqui ok
+			
+			
+			File file = new File(str);
+			
+			try {
+				Scanner scan = new Scanner(file);
+			
+				while(scan.hasNextLine()){   // <<<------solange es eine weitere Zeile gibt dann..
+				System.out.println(scan.nextLine());
+				}
+			}catch(FileNotFoundException e) {
+				System.out.println("Catch");
+				System.out.println(e.getMessage());
+			}
+
+		}
+		
+		
+		
+}
